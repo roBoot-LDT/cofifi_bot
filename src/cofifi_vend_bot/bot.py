eead53332136921fd52d8c64ba90db9c6624df78
Maksim Nemkov <maksim.nemkov.ldt@gmail.com>
2026-03-27 01:11:17 +0300

    'forgot'

--- src/cofifi_vend_bot/bot.py
+++ src/cofifi_vend_bot/bot.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
+import os
 from dotenv import load_dotenv
 from telegram.ext import ApplicationBuilder, CommandHandler
+from telegram.request import HTTPXRequest
 
 from config import TOKEN
 from database import init_db
@@ -11,7 +13,9 @@ load_dotenv()
 
 def main():
     init_db()
-    app = ApplicationBuilder().token(TOKEN).build()
+    proxy = os.getenv("PROXY_URL")  # например socks5://user:pass@host:port
+    request = HTTPXRequest(proxy=proxy) if proxy else HTTPXRequest()
+    app = ApplicationBuilder().token(TOKEN).request(request).build()
 
     app.add_handler(CommandHandler("start", start.start_handler))
     app.add_handler(feedback.conv_handler)
